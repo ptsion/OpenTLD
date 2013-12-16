16403abab1e88abce07dab120c053886c569df1d
Xingtong Zhou <xingtong@67-194-71-95.wireless.umnet.umich.edu>
2013-12-15 23:08:53 -0500

    modified tracker's top impl

--- MedianFlowTracker.h
+++ MedianFlowTracker.h
@@ -1,27 +1,19 @@
-/*  Copyright 2011 AIT Austrian Institute of Technology
-*
-*   This file is part of OpenTLD.
-*
-*   OpenTLD is free software: you can redistribute it and/or modify
-*   it under the terms of the GNU General Public License as published by
-*    the Free Software Foundation, either version 3 of the License, or
-*   (at your option) any later version.
-*
-*   OpenTLD is distributed in the hope that it will be useful,
-*   but WITHOUT ANY WARRANTY; without even the implied warranty of
-*   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-*   GNU General Public License for more details.
-*
-*   You should have received a copy of the GNU General Public License
-*   along with OpenTLD.  If not, see <http://www.gnu.org/licenses/>.
-*
-*/
-
 /*
- * MedianFlowTracker.h
+ *   This file is part of OpenTLD.
  *
- *  Created on: Nov 17, 2011
- *      Author: Georg Nebehay
+ *   OpenTLD is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *    the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   OpenTLD is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with OpenTLD.  If not, see <http://www.gnu.org/licenses/>.
+ *
  */
 
 #ifndef MEDIANFLOWTRACKER_H_
@@ -32,15 +24,14 @@
 namespace tld
 {
 
-class MedianFlowTracker
-{
+class MedianFlowTracker {
 public:
-    cv::Rect *trackerBB;
+	cv::Rect *trackerBB;
 
-    MedianFlowTracker();
-    virtual ~MedianFlowTracker();
-    void cleanPreviousData();
-    void track(const cv::Mat &prevImg, const cv::Mat &currImg, cv::Rect *prevBB, bool showResult = false);
+	MedianFlowTracker();
+	virtual ~MedianFlowTracker();
+	void cleanPreviousData();
+	void track(const cv::Mat &prevImg, const cv::Mat &currImg, cv::Rect *prevBB, bool showResult = false);
 };
 
 } /* namespace tld */
